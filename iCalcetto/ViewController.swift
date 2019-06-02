2ec43521a793103919a9b345ce77b84af0912304
carlo.stringaro@gmail.com <carlo.stringaro@gmail.com>
2019-06-02 12:37:56 +0200

    Initial Commit

---
+++ iCalcetto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iCalcetto
+//
+//  Created by Carlo Stringaro on 02/06/2019.
+//  Copyright © 2019 Carlo Stringaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
